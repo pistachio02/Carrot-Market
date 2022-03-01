9023d440991fa9623196851bfa15848becfbeb87
pistachio02 <wiaptm0219@gmail.com>
2022-03-01 22:54:24 +0900

    CM

--- tailwind-css-2/pages/chats/index.tsx
+++ tailwind-css-2/pages/chats/index.tsx
@@ -1,23 +1,26 @@
 import type { NextPage } from "next";
+import Layout from "../../components/layout";
 
 const Chats: NextPage = () => {
     return (
-        <div className="py-10 divide-y-[1px] ">
-            {[1, 2, 3, 4, 5, 6, 7].map((_, i) => (
-                <div
-                key={i}
-                className="flex px-4 cursor-pointer py-3 items-center space-x-3"
-                >
-                    <div className="w-12 h-12 rounded-full bg-slate-300" />
-                    <div>
-                        <p className="text-gray-700">Steve Jebs</p>
-                        <p className="text-sm  text-gray-500">
-                        See you tomorrow in the corner at 2pm!
-                        </p>
+        <Layout hasTabBar title="채팅">
+            <div className="py-10 divide-y-[1px] ">
+                {[1, 2, 3, 4, 5, 6, 7].map((_, i) => (
+                    <div
+                    key={i}
+                    className="flex px-4 cursor-pointer py-3 items-center space-x-3"
+                    >
+                        <div className="w-12 h-12 rounded-full bg-slate-300" />
+                        <div>
+                            <p className="text-gray-700">Steve Jebs</p>
+                            <p className="text-sm  text-gray-500">
+                            See you tomorrow in the corner at 2pm!
+                            </p>
+                        </div>
                     </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+        </Layout>
     );
 };
 
