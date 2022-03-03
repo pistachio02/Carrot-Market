dc28a79e29009951d3e1a4fa0ea60525e10fc849
pistachio02 <wiaptm0219@gmail.com>
2022-03-03 16:22:38 +0900

    CM

---
+++ Prisma-1/pages/api/users/me.tsx
@@ -0,0 +1,25 @@
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseType>
+) {
+   
+    // console.log(req.session.user);
+
+    const profile = await client.user.findUnique({
+        where: {
+            id: req.session.user?.id
+        },
+    });
+    // console.log(profile)
+    res.json({
+        ok: true,
+        profile,
+    })
+};
+
+export default withApiSession(withHandler("GET", handler));
