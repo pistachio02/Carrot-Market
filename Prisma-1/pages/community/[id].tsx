a4fe6b885b06fb2d5647e461c8a6a28926a14bd2
pistachio02 <wiaptm0219@gmail.com>
2022-03-04 18:45:41 +0900

    CM

--- Prisma-1/pages/community/[id].tsx
+++ Prisma-1/pages/community/[id].tsx
@@ -70,8 +70,9 @@ const CommunityPostDetail: NextPage = () => {
   useEffect(() => {
     if (answerData && answerData.ok) {
       reset();
+      mutate();
     }
-  }, [answerData, reset]);
+  }, [answerData, reset, mutate]);
 
   return (
     <Layout canGoBack>
