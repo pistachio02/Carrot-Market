bb6b523741f3001e7d7712c6fc5da2cd10432ce3
pistachio02 <wiaptm0219@gmail.com>
2022-03-04 15:13:37 +0900

    CM

--- Prisma-1/pages/products/[id].tsx
+++ Prisma-1/pages/products/[id].tsx
@@ -25,7 +25,9 @@ const ItemDetail: NextPage = () => {
   const { data, mutate } = useSWR<ItemDetailResponse>(router.query.id ? `/api/products/${router.query.id}` : null)
   const [toggleFav] = useMutation(`/api/products/${router.query.id}/fav`);
   const onFavClick = () => {
-    // toggleFav({});
+    if(!data) return;
+    mutate({ ...data, isLiked: !data.isLiked }, false);
+    toggleFav({});
   }
 
   return (
