192fceb2091ae17f5804f1cfb11cc6fe7b527485
pistachio02 <wiaptm0219@gmail.com>
2022-03-04 14:58:26 +0900

    CM

--- Prisma-1/pages/api/products/[id]/index.ts
+++ Prisma-1/pages/api/products/[id]/index.ts
@@ -7,7 +7,7 @@ async function handler(
     req: NextApiRequest,
     res: NextApiResponse<ResponseType>
 ) {
-    const { id } = req.query;
+    const { query: { id }, session: { user } } = req;
     const product = await client.product.findUnique({
         where: {
             id: +id.toString(),
@@ -41,10 +41,23 @@ async function handler(
     });
     // console.log(relatedProducts)
 
+    const isLiked = Boolean(
+        await client.fav.findFirst({
+            where: {
+                productId: product?.id,
+                userId: user?.id
+            },
+            select: {
+                id: true,
+            },
+        })
+    );
+
     res.json({
         ok: true,
         product,
         relatedProducts,
+        isLiked,
     });
 };
 
