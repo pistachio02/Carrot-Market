d4371a1668752589e8c1702df4a0755608b2876a
pistachio02 <wiaptm0219@gmail.com>
2022-03-06 15:33:25 +0900

    CM

--- Prisma-1/pages/profile/edit.tsx
+++ Prisma-1/pages/profile/edit.tsx
@@ -2,35 +2,43 @@ import type { NextPage } from "next";
 import Button from "@components/button";
 import Input from "@components/input";
 import Layout from "@components/layout";
+import useUser from "@libs/client/useUser";
 import { useForm } from "react-hook-form";
+import { useEffect } from "react";
 
 interface EditProfileForm {
   email?: string;
   phone?: string;
-  name?: string;
-  avatar?: FileList;
   formErrors?: string;
-}
-
-interface EditProfileResponse {
-  ok: boolean;
-  error?: string;
 }
 
 const EditProfile: NextPage = () => {
 
+  const { user } = useUser();
   const {
     register,
     setValue,
     handleSubmit,
     setError,
     formState: { errors },
-    watch,
   } = useForm<EditProfileForm>();
+
+  useEffect(() => {
+    if (user?.email) setValue("email", user.email);
+    if (user?.phone) setValue("phone", user.phone);
+  }, [user, setValue]);
+
+  const onValid = ({ email, phone }: EditProfileForm) => {
+    if (email === "" && phone === "") {
+      setError("formErrors", {
+        message: "E-mail or Phone number are required. You need to choose one.",
+      });
+    }
+  };
 
   return (
     <Layout canGoBack title="Edit Profile">
-      <form className="py-10 px-4 space-y-4">
+      <form onSubmit={handleSubmit(onValid)} className="py-10 px-4 space-y-4">
         <div className="flex items-center space-x-3">
           <div className="w-14 h-14 rounded-full bg-slate-500" />
           <label
@@ -38,8 +46,7 @@ const EditProfile: NextPage = () => {
             className="cursor-pointer py-2 px-3 border hover:bg-gray-50 border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
           >
             Change
-            <Input
-              {...register("avatar")}
+            <input
               id="picture"
               type="file"
               className="hidden"
@@ -49,18 +56,24 @@ const EditProfile: NextPage = () => {
         </div>
         <Input
           register={register("email")}
-          required
+          required={false}
           label="Email address"
           name="email"
-          type="email" />
+          type="email"
+        />
         <Input
           register={register("phone")}
-          required
+          required={false}
           label="Phone number"
           name="phone"
-          type="number"
+          type="text"
           kind="phone"
         />
+        {errors.formErrors ? (
+          <span className="my-2 text-red-500 font-medium text-center block">
+            {errors.formErrors.message}
+          </span>
+        ) : null}
         <Button text="Update profile" />
       </form>
     </Layout>
