a7315ca0b64c528839a079cb6ac6672f1bf24d3a
pistachio02 <wiaptm0219@gmail.com>
2022-03-07 16:48:22 +0900

    CM

--- Prisma-1/pages/streams/[id].tsx
+++ Prisma-1/pages/streams/[id].tsx
@@ -76,9 +76,18 @@ const Stream: NextPage = () => {
         <div className="mt-5">
           <h1 className="text-3xl font-bold text-gray-900">{data?.stream?.name}</h1>
           <span className="text-2xl block mt-3 text-gray-900">$ {data?.stream?.price}</span>
-          <p className=" my-6 text-gray-700">
-            {data?.stream?.description}
-          </p>
+          <p className=" my-6 text-gray-700">{data?.stream?.description}</p>
+          <div className="bg-orange-400 p-5 rounded-md overflow-scroll flex flex-col space-y-3">
+            <span>Stream Keys (Secret)</span>
+            <span className="text-white">
+              <span className="font-medium text-gray-800">URL:</span>{" "}
+              {data?.stream.cloudflareUrl}
+            </span>
+            <span className="text-white">
+              <span className="font-medium text-gray-800">Key:</span>{" "}
+              {data?.stream.cloudflareKey}
+            </span>
+          </div>
         </div>
         <div>
           <h2 className="text-2xl font-bold text-gray-900">Live Chat</h2>
