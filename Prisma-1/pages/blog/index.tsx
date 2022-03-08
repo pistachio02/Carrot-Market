6df8af8424d89125508af8e4601ff229db64c12c
pistachio02 <wiaptm0219@gmail.com>
2022-03-08 17:58:55 +0900

    CM

--- Prisma-1/pages/blog/index.tsx
+++ Prisma-1/pages/blog/index.tsx
@@ -2,21 +2,31 @@ import Layout from "@components/layout";
 import matter from "gray-matter";
 import { readdirSync, readFileSync } from "fs";
 import { NextPage } from "next";
+import Link from "next/link";
 
 interface Post {
     title: string;
     date: string;
     category: string;
+    slug: string;
 }
 
 const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
-    return(
+    return (
         <Layout title="Blog">
             <h1 className="font-semibold text-lg text-center mt-5 mb-10">Latest Posts : </h1>
             {posts.map((post, index) => (
                 <div key={index} className="mb-5 mt-5">
-                    <span className="text-lg text-red-500">{post.title}</span>
-                    <div><span>{post.date} / {post.category}</span></div>
+                    <Link href={`/blog/${post.slug}`}>
+                        <a>
+                            <span className="text-lg text-red-500">{post.title}</span>
+                            <div>
+                                <span>
+                                    {post.date} / {post.category}
+                                </span>
+                            </div>
+                        </a>
+                    </Link>
                 </div>
             ))}
         </Layout>
@@ -26,9 +36,9 @@ const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
 export async function getStaticProps() {
     const blogPosts = readdirSync("./posts").map((file) => {
         const content = readFileSync(`./posts/${file}`, "utf-8");
-        return matter(content).data;
-    })
-    // console.log(blogPosts)
+        const [slug, _] = file.split(".");
+        return { ...matter(content).data, slug };
+    });
     return {
         props: {
             posts: blogPosts,
